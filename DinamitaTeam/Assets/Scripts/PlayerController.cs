fa43ce0bd4bee8fc249d53707421b007cfe814a5
Hernán Hernández <hernanhernandezingeniero@gmail.com>
2023-02-03 22:34:01 -0500

    The player collision now don't shake

--- DinamitaTeam/Assets/Scripts/PlayerController.cs
+++ DinamitaTeam/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     public float xVelocity = 10.0f;
 
+    private Rigidbody2D rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 translation = new Vector3();
-
+        // Option #1
+        /*Vector3 translation = new Vector3();
         if (Input.GetKey(KeyCode.RightArrow))
         {
             translation = new Vector3(xVelocity * Time.deltaTime, 0.0f, 0.0f);
@@ -26,6 +28,23 @@ public class PlayerController : MonoBehaviour
         {
             translation = new Vector3(-xVelocity * Time.deltaTime, 0.0f, 0.0f);
             transform.Translate(translation);
-        } 
+        }*/
+
+        // Option #2
+        /*var xAxis = Input.GetAxis("Horizontal");
+        transform.Translate(xAxis * xVelocity * Time.deltaTime, 0.0f, 0.0f);*/
+
+        // Option #3
+        Vector3 translation = new Vector3();
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            translation = new Vector3(xVelocity, 0.0f, 0.0f);
+            rigidBody.AddForce(translation);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            translation = new Vector3(-xVelocity, 0.0f, 0.0f);
+            rigidBody.AddForce(translation);
+        }
     }
 }
