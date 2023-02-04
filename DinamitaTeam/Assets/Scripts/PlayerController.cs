583b5b1ad198bddf856bdd339789ba474a71264d
Hernán Hernández <hernanhernandezingeniero@gmail.com>
2023-02-04 00:46:38 -0500

    Animation created and mirror implemented

--- DinamitaTeam/Assets/Scripts/PlayerController.cs
+++ DinamitaTeam/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     public float xVelocity = 10.0f;
 
     private Rigidbody2D rigidBody;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+        animator = this.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -39,16 +41,21 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             translation = new Vector3(xVelocity, 0.0f, 0.0f);
+            transform.eulerAngles = new Vector2(0, 0);
+            animator.SetBool("Walking",true);
             rigidBody.AddForce(translation);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             translation = new Vector3(-xVelocity, 0.0f, 0.0f);
+            transform.eulerAngles = new Vector2(0, 180);
+            animator.SetBool("Walking", true);
             rigidBody.AddForce(translation);
         }
         else
         {
             translation = Vector3.zero;
+            animator.SetBool("Walking", false);
             rigidBody.velocity = translation;
         }
     }
